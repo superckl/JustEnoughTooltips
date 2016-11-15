9f55f0febbd6f1b27411b9f825f1efd260f9d4e7
superckl <superckl@superckl.me>
2016-11-15 01:25:43 -0600

    Update to latest JEI and create our own RecipeGuiLogic

---
+++ src/main/java/me/superckl/jet/RecipeGuiLogic.java
@@ -0,0 +1,175 @@
+package me.superckl.jet;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
+import com.google.common.collect.ImmutableList;
+
+import me.superckl.jet.integration.JEIIntegrationModule;
+import mezz.jei.api.IRecipeRegistry;
+import mezz.jei.api.gui.IRecipeLayoutDrawable;
+import mezz.jei.api.recipe.IFocus;
+import mezz.jei.api.recipe.IFocus.Mode;
+import mezz.jei.api.recipe.IRecipeCategory;
+import mezz.jei.api.recipe.IRecipeWrapper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.inventory.Container;
+
+/**
+ * Derived from {@link mezz.jei.gui.recipes.RecipeGuiLogic} to have only desired functionality.
+ * Uses only API classes.
+ */
+public class RecipeGuiLogic {
+
+	private static class State {
+		@Nonnull
+		public final IFocus<?> focus;
+		@Nonnull
+		public final ImmutableList<IRecipeCategory> recipeCategories;
+
+		public int recipeCategoryIndex;
+		public int pageIndex;
+
+		public List<IRecipeLayoutDrawable> layouts;
+
+		public State(final IFocus<?> focus, final List<IRecipeCategory> recipeCategories, final int recipeCategoryIndex, final int pageIndex) {
+			this.focus = focus;
+			this.recipeCategories = ImmutableList.copyOf(recipeCategories);
+			this.recipeCategoryIndex = recipeCategoryIndex;
+			this.pageIndex = pageIndex;
+		}
+	}
+
+	@Nonnull
+	private final IRecipeRegistry recipeRegistry;
+
+	@Nonnull
+	private State state;
+
+	/**
+	 * List of recipes for the currently selected recipeClass
+	 */
+	private List<IRecipeWrapper> recipes = Collections.emptyList();
+
+	public RecipeGuiLogic(final IRecipeRegistry recipeRegistry) {
+		this.recipeRegistry = recipeRegistry;
+		final IFocus focus = recipeRegistry.createFocus(IFocus.Mode.NONE, null);
+		final List<IRecipeCategory> recipeCategories = recipeRegistry.getRecipeCategories();
+		this.state = new State(focus, recipeCategories, 0, 0);
+	}
+
+	public <V> boolean setFocus(final IFocus<V> focus) {
+		return this.setFocus(focus, false);
+	}
+
+	private <V> boolean setFocus(final IFocus<V> focus, final boolean force){
+		final List<IRecipeCategory> recipeCategories = this.recipeRegistry.getRecipeCategories(focus);
+		if (recipeCategories.isEmpty() && !force)
+			return false;
+
+		final int recipeCategoryIndex = this.getRecipeCategoryIndex(recipeCategories);
+
+		final State state = new State(focus, recipeCategories, recipeCategoryIndex, 0);
+		this.setState(state);
+
+		return true;
+	}
+
+	private int getRecipeCategoryIndex(final List<IRecipeCategory> recipeCategories) {
+		final Container container = Minecraft.getMinecraft().thePlayer.openContainer;
+		if (container == null)
+			return 0;
+
+		for (int i = 0; i < recipeCategories.size(); i++) {
+			final IRecipeCategory recipeCategory = recipeCategories.get(i);
+			if (this.recipeRegistry.getRecipeTransferHandler(container, recipeCategory) != null)
+				return i;
+		}
+
+		return 0;
+	}
+
+	private void setState(final State state) {
+		this.state = state;
+		this.updateRecipes();
+	}
+
+	private void updateRecipes() {
+		if(this.state.recipeCategories.isEmpty()){
+			this.recipes = Collections.EMPTY_LIST;
+			this.state.layouts = Collections.EMPTY_LIST;
+			return;
+		}
+		final IRecipeCategory recipeCategory = this.getSelectedRecipeCategory();
+		final IFocus<?> focus = this.state.focus;
+		//noinspection unchecked
+		this.recipes = this.recipeRegistry.getRecipeWrappers(recipeCategory, focus);
+		this.state.layouts = this.getRecipeLayouts();
+	}
+
+
+	public IRecipeCategory getSelectedRecipeCategory() {
+		return this.state.recipeCategories.get(this.state.recipeCategoryIndex);
+	}
+
+	public void nextPage() {
+		this.state.pageIndex = (this.state.pageIndex + 1) % this.state.layouts.size();
+	}
+
+	public void previousPage() {
+		this.state.pageIndex = (this.state.layouts.size() + this.state.pageIndex - 1) % this.state.layouts.size();
+	}
+
+	public boolean hasMultipleCategories() {
+		return this.state.recipeCategories.size() > 1;
+	}
+
+	private List<IRecipeLayoutDrawable> getRecipeLayouts() {
+		final List<IRecipeLayoutDrawable> recipeLayouts = new ArrayList<IRecipeLayoutDrawable>();
+
+		if(this.state.recipeCategories.isEmpty())
+			return recipeLayouts;
+		final IRecipeCategory recipeCategory = this.getSelectedRecipeCategory();
+
+		final int recipeWidgetIndex = 0;
+		for (int recipeIndex = 0; recipeIndex < this.recipes.size(); recipeIndex++) { //Replaced number of pages with hard-coded 1
+			final IRecipeWrapper recipeWrapper = this.recipes.get(recipeIndex);
+			if (recipeWrapper == null)
+				continue;
+			final IRecipeLayoutDrawable layout = JEIIntegrationModule.jeiRuntime.getRecipeRegistry().createRecipeLayoutDrawable(recipeCategory, recipeWrapper, this.state.focus);
+			layout.setPosition(0, 0);
+			recipeLayouts.add(layout);
+		}
+
+		return recipeLayouts;
+	}
+
+	public IRecipeLayoutDrawable getCurrentLayout(){
+		return this.state.layouts.isEmpty() ? null:this.state.layouts.get(this.state.pageIndex);
+	}
+
+	public void nextRecipeCategory() {
+		final int recipesTypesCount = this.state.recipeCategories.size();
+		this.state.recipeCategoryIndex = (this.state.recipeCategoryIndex + 1) % recipesTypesCount;
+		this.state.pageIndex = 0;
+		this.updateRecipes();
+	}
+
+	public boolean nextMode(){
+		final Mode newMode = this.state.focus.getMode() == Mode.INPUT ? Mode.OUTPUT:Mode.INPUT;
+		return this.setFocus(JEIIntegrationModule.jeiRuntime.getRecipeRegistry().createFocus(newMode, this.state.focus.getValue()));
+	}
+
+	public <V> void newFocusItem(final V focusItem){
+		if(!this.setFocus(JEIIntegrationModule.jeiRuntime.getRecipeRegistry().createFocus(Mode.OUTPUT, focusItem)))
+			this.setFocus(JEIIntegrationModule.jeiRuntime.getRecipeRegistry().createFocus(Mode.INPUT, focusItem), true);
+	}
+
+	public IFocus getFocus() {
+		return this.state.focus;
+	}
+
+}
--- src/main/java/me/superckl/jet/handler/RenderTickHandler.java
+++ src/main/java/me/superckl/jet/handler/RenderTickHandler.java
@@ -1,29 +1,20 @@
 package me.superckl.jet.handler;
 
-import java.util.List;
-
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 
 import me.superckl.jet.Config;
 import me.superckl.jet.KeyBindings;
+import me.superckl.jet.RecipeGuiLogic;
 import me.superckl.jet.integration.JEIIntegrationModule;
 import me.superckl.jet.util.LogHelper;
 import me.superckl.jet.util.RecipeDrawingException;
 import me.superckl.jet.util.RenderHelper;
-import mezz.jei.RecipeRegistry;
 import mezz.jei.api.IRecipesGui;
-import mezz.jei.api.recipe.IFocus;
 import mezz.jei.api.recipe.IFocus.Mode;
 import mezz.jei.api.recipe.transfer.IRecipeTransferError;
-import mezz.jei.gui.IRecipeGuiLogic;
-import mezz.jei.gui.RecipeGuiLogic;
-import mezz.jei.gui.RecipeLayout;
-import mezz.jei.plugins.vanilla.furnace.SmeltingRecipe;
-import mezz.jei.transfer.RecipeTransferUtil;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.client.renderer.GlStateManager;
@@ -44,11 +35,8 @@ import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
 public class RenderTickHandler {
 
 	private final Minecraft mc = FMLClientHandler.instance().getClient();
-	private IRecipeGuiLogic logic;
-	private Mode mode = Mode.OUTPUT;
+	private RecipeGuiLogic logic;
 	private ItemStack lastStack;
-	private RecipeLayout layout;
-	private boolean needsReset;
 	private IRecipeTransferError error;
 
 	@SubscribeEvent
@@ -56,7 +44,7 @@ public class RenderTickHandler {
 		if(!Config.renderInGame || this.mc.currentScreen != null || e.getType() != ElementType.CROSSHAIRS || !KeyBindings.DISPLAY_1.isKeyDown())
 			return;
 		if(this.logic == null)
-			this.logic = new RecipeGuiLogic((RecipeRegistry) JEIIntegrationModule.jeiRuntime.getRecipeRegistry());
+			this.logic = new RecipeGuiLogic(JEIIntegrationModule.jeiRuntime.getRecipeRegistry());
 		ItemStack toCheck = null;
 		if(!this.mc.thePlayer.isSneaking() && this.mc.thePlayer.getHeldItem(EnumHand.MAIN_HAND) != null)
 			toCheck = this.mc.thePlayer.getHeldItem(EnumHand.MAIN_HAND);
@@ -73,12 +61,12 @@ public class RenderTickHandler {
 		final float scale = Config.scaleInGame;
 		int x = Math.round(e.getResolution().getScaledWidth()/2);
 		int y = e.getResolution().getScaledHeight()/2+13;
-		this.checkLastItem(toCheck, x, y);
-		if(this.layout != null){
-			int width = this.logic.getRecipeCategory().getBackground().getWidth();
-			final int height = this.logic.getRecipeCategory().getBackground().getHeight();
-			if(this.layout.getRecipeWrapper() instanceof SmeltingRecipe)
-				width += 8;
+		this.checkLastItem(toCheck);
+		if(this.logic.getCurrentLayout() != null){
+			final int width = this.logic.getSelectedRecipeCategory().getBackground().getWidth();
+			final int height = this.logic.getSelectedRecipeCategory().getBackground().getHeight();
+			/*if(this.layout.getClass() instanceof SmeltingRecipe)
+				width += 8;*/ //TODO broken now
 			GlStateManager.pushMatrix();
 			GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 			GlStateManager.disableLighting();
@@ -103,19 +91,17 @@ public class RenderTickHandler {
 			RenderHelper.fillGuiArea(x, y, 500, width, height, scale);
 			GlStateManager.popMatrix();
 
-			this.layout.getRecipeTransferButton().enabled = false;
-			this.layout.getRecipeTransferButton().visible = false;
 			GlStateManager.pushMatrix();
 			GlStateManager.scale(scale, scale, 1F);
 			//Translate to move the draw to the right spot. The x and y passed on creation of the layouts may not be accurate (resizing, position overrides, etc.)
-			GlStateManager.translate(x/scale-this.layout.getPosX(), y/scale-this.layout.getPosY(), 501F);
+			GlStateManager.translate(x/scale/*-this.layout.getPosX()*/, y/scale/*-this.layout.getPosY()*/, 501F);
 			//final JERScissorHook hook = JustEnoughTooltips.instance.getScissorHook();
 			//hook.setScale(scale).setX(x).setY(y).setResolution(e.getResolution()).setHeight(height).setWidth(width).setApply(true);
 			try{
 				//Fake mouse parameters, middle of recipe layout
-				this.layout.draw(this.mc, Math.round(this.layout.getPosX()+width/2), Math.round(this.layout.getPosY()+height/2));
+				this.logic.getCurrentLayout().draw(this.mc, Math.round(/*this.layout.getPosX()+*/width/2), Math.round(/*this.layout.getPosY()+*/height/2));
 			}catch(final Exception e1){
-				throw new RecipeDrawingException("An error ocurred while drawing a recipe with category: "+this.layout.getRecipeCategory().getTitle(), e1);
+				throw new RecipeDrawingException("An error ocurred while drawing a recipe with category: "+this.logic.getSelectedRecipeCategory().getTitle(), e1);
 			}
 			//hook.setApply(false);
 			GlStateManager.popMatrix();
@@ -125,37 +111,31 @@ public class RenderTickHandler {
 
 	@SubscribeEvent
 	public void onMouseInput(final MouseEvent e){
-		if(this.layout == null || !KeyBindings.DISPLAY_1.isKeyDown())
+		if(this.logic == null || this.logic.getCurrentLayout() == null || !KeyBindings.DISPLAY_1.isKeyDown())
 			return;
 		if(this.logic == null)
-			this.logic = new RecipeGuiLogic((RecipeRegistry) JEIIntegrationModule.jeiRuntime.getRecipeRegistry());
+			this.logic = new RecipeGuiLogic(JEIIntegrationModule.jeiRuntime.getRecipeRegistry());
 		if(e.getDwheel() != 0){
 			e.setCanceled(true);
-			if(e.getDwheel() > 0){
+			if(e.getDwheel() > 0)
 				this.logic.nextPage();
-				this.needsReset = true;
-			}else{
+			else
 				this.logic.previousPage();
-				this.needsReset = true;
-			}
 		}
 	}
 
 	@SubscribeEvent
 	public void onMouseInput2(final MouseInputEvent e){
-		if(this.layout == null || !Keyboard.isKeyDown(KeyBindings.DISPLAY_1.getKeyCode()))
+		if(this.logic == null || this.logic.getCurrentLayout() == null || !Keyboard.isKeyDown(KeyBindings.DISPLAY_1.getKeyCode()))
 			return;
 		if(this.logic == null)
-			this.logic = new RecipeGuiLogic((RecipeRegistry) JEIIntegrationModule.jeiRuntime.getRecipeRegistry());
+			this.logic = new RecipeGuiLogic(JEIIntegrationModule.jeiRuntime.getRecipeRegistry());
 		if(Mouse.getEventDWheel() != 0){
 			e.setCanceled(true);
-			if(Mouse.getEventDWheel() > 0){
+			if(Mouse.getEventDWheel() > 0)
 				this.logic.nextPage();
-				this.needsReset = true;
-			}else{
+			else
 				this.logic.previousPage();
-				this.needsReset = true;
-			}
 		}
 	}
 
@@ -163,35 +143,30 @@ public class RenderTickHandler {
 	public void onKeyPress(final KeyInputEvent e){
 		if(!Keyboard.getEventKeyState())
 			return;
-		if(this.layout == null || !Keyboard.isKeyDown(KeyBindings.DISPLAY_1.getKeyCode()))
+		if(this.logic == null || this.logic.getCurrentLayout() == null || !Keyboard.isKeyDown(KeyBindings.DISPLAY_1.getKeyCode()))
 			return;
 		if(this.logic == null)
-			this.logic = new RecipeGuiLogic((RecipeRegistry) JEIIntegrationModule.jeiRuntime.getRecipeRegistry());
+			this.logic = new RecipeGuiLogic(JEIIntegrationModule.jeiRuntime.getRecipeRegistry());
 		final int key = Keyboard.getEventKey();
 		if(KeyBindings.NEXT_CATEGORY.getKeyCode() == key){
-			if(this.logic.hasMultipleCategories()){
+			if(this.logic.hasMultipleCategories())
 				this.logic.nextRecipeCategory();
-				this.needsReset = true;
-			}
-		}else if(KeyBindings.SWITCH_USES_RECIPES.getKeyCode() == key){
-			this.mode = this.mode == Mode.OUTPUT ? Mode.INPUT:Mode.OUTPUT;
-			this.needsReset = true;
-		}else if(this.mc.currentScreen == null && mezz.jei.config.KeyBindings.showRecipe.getKeyCode() == key)
+		}else if(KeyBindings.SWITCH_USES_RECIPES.getKeyCode() == key)
+			this.logic.nextMode();
+		else if(this.mc.currentScreen == null && mezz.jei.config.KeyBindings.showRecipe.getKeyCode() == key)
 			try {
 				this.getRecipesGui();
 				if(this.lastStack != null){
 					this.mc.displayGuiScreen(new GuiInventory(this.mc.thePlayer));
-					if(this.mode == Mode.OUTPUT)
-						this.getRecipesGui().showRecipes(this.lastStack);
-					else
-						this.getRecipesGui().showUses(this.lastStack);
+					this.getRecipesGui().show(this.logic.getFocus());
 				}
 			} catch (final Exception e1) {
 				LogHelper.error("An error occurred when opening the recipes gui!");
 				e1.printStackTrace();
 			}
-		else if(this.mc.thePlayer != null && this.mc.thePlayer.openContainer != null && this.error == null && KeyBindings.FILL_RECIPE.getKeyCode() == key)
-			RecipeTransferUtil.transferRecipe(this.mc.thePlayer.openContainer, this.layout, this.mc.thePlayer, GuiScreen.isShiftKeyDown());
+		/*else if(this.mc.thePlayer != null && this.mc.thePlayer.openContainer != null && this.error == null && KeyBindings.FILL_RECIPE.getKeyCode() == key)
+			RecipeTransferUtil.transferRecipe(this.mc.thePlayer.openContainer, this.layout, this.mc.thePlayer, GuiScreen.isShiftKeyDown());*/
+		//TODO Removed transfer functionality
 	}
 
 	@SubscribeEvent //fired while in GUI, but isPressed returns false
@@ -206,19 +181,19 @@ public class RenderTickHandler {
 		if(this.mc.currentScreen == this.getRecipesGui())
 			return;
 		if(this.logic == null)
-			this.logic = new RecipeGuiLogic((RecipeRegistry) JEIIntegrationModule.jeiRuntime.getRecipeRegistry());
+			this.logic = new RecipeGuiLogic(JEIIntegrationModule.jeiRuntime.getRecipeRegistry());
 		final ScaledResolution resolution = new ScaledResolution(this.mc);
 		final float scale = Config.scaleInTooltip;
 		final int mouseX = Math.round(Mouse.getEventX() / resolution.getScaleFactor());
 		final int mouseY = (this.mc.displayHeight - Mouse.getEventY()) / resolution.getScaleFactor();
 		int x = mouseX-8;
 		int y = mouseY;
-		this.checkLastItem(e.getItemStack(), x, y);
-		if(this.layout != null){
-			int width = this.logic.getRecipeCategory().getBackground().getWidth();
-			final int height = this.logic.getRecipeCategory().getBackground().getHeight();
-			if(this.layout.getRecipeWrapper() instanceof SmeltingRecipe)
-				width += 8;
+		this.checkLastItem(e.getItemStack());
+		if(this.logic.getCurrentLayout() != null){
+			final int width = this.logic.getSelectedRecipeCategory().getBackground().getWidth();
+			final int height = this.logic.getSelectedRecipeCategory().getBackground().getHeight();
+			/*if(this.layout.getRecipeWrapper() instanceof SmeltingRecipe)
+				width += 8;*/ //TODO broken now
 			GlStateManager.pushMatrix();
 			GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 			GlStateManager.disableLighting();
@@ -243,22 +218,20 @@ public class RenderTickHandler {
 			RenderHelper.fillGuiArea(x, y, 500, width, height, scale);
 			GlStateManager.popMatrix();
 
-			this.layout.getRecipeTransferButton().enabled = false;
-			this.layout.getRecipeTransferButton().visible = false;
 			GlStateManager.pushMatrix();
 			GlStateManager.scale(scale, scale, 1F);
 			//Translate to move the draw to the right spot. The x and y passed on creation of the layouts may not be accurate (resizing, position overrides, etc.)
-			final float xDiff = x/scale-this.layout.getPosX();
-			final float yDiff = y/scale-this.layout.getPosY();
+			final float xDiff = x/scale/*-this.layout.getPosX()*/;
+			final float yDiff = y/scale/*-this.layout.getPosY()*/;
 			GlStateManager.translate(xDiff, yDiff, 501F);
 			//final JERScissorHook hook = JustEnoughTooltips.instance.getScissorHook();
 			//hook.setScale(scale).setX(x).setY(y).setResolution(resolution).setHeight(height).setWidth(width).setApply(true);
 			try{
-				this.layout.draw(this.mc, Math.round(mouseX-xDiff), Math.round(mouseY - yDiff));
+				this.logic.getCurrentLayout().draw(this.mc, Math.round(mouseX-xDiff), Math.round(mouseY - yDiff));
 				if(this.mc.thePlayer.openContainer != null && this.error != null && Keyboard.isKeyDown(KeyBindings.FILL_RECIPE.getKeyCode()))
-					this.error.showError(this.mc, Math.round(x/scale+this.layout.getPosX()-x/scale-12),Math.round(y/scale+this.layout.getPosY()-y/scale-5), this.layout, x , y);
+					this.error.showError(this.mc, Math.round(x/scale+/*this.layout.getPosX()-x/scale*/-12),Math.round(y/scale/*+this.layout.getPosY()-y/scale*/-5), this.logic.getCurrentLayout(), x , y);
 			}catch(final Exception e1){
-				throw new RecipeDrawingException("An error ocurred while drawing a recipe with category: "+this.layout.getRecipeCategory().getTitle(), e1);
+				throw new RecipeDrawingException("An error ocurred while drawing a recipe with category: "+this.logic.getSelectedRecipeCategory().getTitle(), e1);
 			}
 			//hook.setApply(false);
 			GlStateManager.popMatrix();
@@ -266,40 +239,13 @@ public class RenderTickHandler {
 		this.lastStack = e.getItemStack();
 	}
 
-	private void checkLastItem(final ItemStack toCheck, final int x, final int y){
-		final boolean changedStacks = (this.lastStack != null && !toCheck.isItemEqual(this.lastStack));
+	private void checkLastItem(final ItemStack toCheck){
+		final boolean changedStacks = (this.lastStack == null && toCheck != null) || (this.lastStack != null && !toCheck.isItemEqual(this.lastStack));
 		if(changedStacks)
-			this.mode = Mode.OUTPUT;
-		this.tempMode = this.mode;
-		if(this.needsReset || changedStacks || this.lastStack == null)
-			this.resetGuiLogic(toCheck, x, y);
+			this.logic.newFocusItem(toCheck);
 	}
 
 	private Mode tempMode;
-
-	private void resetGuiLogic(final ItemStack toCheck, final int x, final int y){
-		if(this.logic == null)
-			this.logic = new RecipeGuiLogic((RecipeRegistry) JEIIntegrationModule.jeiRuntime.getRecipeRegistry());
-		this.needsReset = false;
-		final IFocus<ItemStack> focus = JEIIntegrationModule.jeiRuntime.getRecipeRegistry().createFocus(this.mode, toCheck);
-		if(!this.logic.setFocus(focus)){
-			this.layout = null;
-			this.error = null;
-			if(this.tempMode != this.mode)
-				return;
-			this.tempMode = this.mode;
-			this.mode = this.mode == Mode.INPUT ? Mode.OUTPUT:Mode.INPUT;
-			this.resetGuiLogic(toCheck, x, y);
-			return;
-		}
-		this.logic.setRecipesPerPage(1);
-		final List<RecipeLayout> layouts = this.logic.getRecipeWidgets(x, y, 0);
-		this.layout = layouts.isEmpty() ? null:layouts.get(0);
-		if(this.layout != null && this.mc.thePlayer.openContainer != null)
-			this.error = RecipeTransferUtil.getTransferRecipeError(this.mc.thePlayer.openContainer, this.layout, this.mc.thePlayer);
-		else if(this.layout == null)
-			this.error = null;
-	}
 
 	private IRecipesGui getRecipesGui(){
 		return JEIIntegrationModule.jeiRuntime == null ? null:JEIIntegrationModule.jeiRuntime.getRecipesGui();
